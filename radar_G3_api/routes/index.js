a93482b430d0d1646da651b5551ad461678c69e6
BeatrizMiotto <miottobiasforever@gmail.com>
2022-11-28 20:43:19 -0300

    atualização da API

--- radar_G3_api/routes/index.js
+++ radar_G3_api/routes/index.js
@@ -5,10 +5,21 @@ const ProdutosControllers = require("../controllers/produtosControllers");
 
 
 
-/* GET home page. */
 
+
+/*Clientes*/
 router.get('/clientes', ClientesControllers.index);
+router.post('/clientes', ClientesControllers.create);
+router.get('/clientes/:id', ClientesControllers.show);
+router.delete('/clientes/:id', ClientesControllers.delete);
+router.put('/clientes/:id', ClientesControllers.update);
+
+/*Produtos*/
 router.get('/produtos', ProdutosControllers.index);
+router.post('/produtos', ProdutosControllers.create);
+router.get('/produtos/:id', ProdutosControllers.show);
+router.delete('/produtos/:id', ProdutosControllers.delete);
+router.put('/produtos/:id', ProdutosControllers.update);
 
 
 module.exports = router;
